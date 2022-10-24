9d4dbd66182c5b86832fb8999530b66581293724
Ruben Röhner <ru.roehner@gmail.com>
2022-10-24 08:46:51 +0200

    react-native lists: add search bar and filte logic

--- 00_LargeAmountsOfData/ReactNative/reactnative_lists/src/screens/MainScreen.tsx
+++ 00_LargeAmountsOfData/ReactNative/reactnative_lists/src/screens/MainScreen.tsx
@@ -1,3 +1,5 @@
+import { SearchBar } from "@rneui/themed";
+import React from "react";
 import { FlatList, SafeAreaView, StatusBar, Text } from "react-native";
 import ComplexListItem from "../components/ComplexListItem";
 import SimpleListItem, { ListItemData } from "../components/SimpleListItem";
@@ -5,31 +7,42 @@ import SimpleListItem, { ListItemData } from "../components/SimpleListItem";
 const itemCount = 100;
 const showSimpleListItem = false
 
-function getListItems() {
-    let items = []
-    for (let i = 0; i < itemCount; i++) {
-        items.push({ title: "Item no." + i, paragraph: "lorem ipsum dolor sit amet", index: i } as ListItemData)
+export default class MainScreen extends React.Component {
+
+    state = {
+        searchValue: ''
     }
-    return items
-}
 
-function renderItem(itemData: ListItemData) {
-    if (showSimpleListItem) {
-        return SimpleListItem(itemData)
-    } else {
-        return ComplexListItem(itemData)
+    getListItems(searchValue: string) {
+        let items = []
+        for (let i = 0; i < itemCount; i++) {
+            items.push({ title: "Item no." + i, paragraph: "lorem ipsum dolor sit amet", index: i } as ListItemData)
+        }
+        return items.filter((itemData: ListItemData) => itemData.title.toLowerCase().includes(searchValue.toLowerCase()) || itemData.paragraph.toLowerCase().includes(searchValue.toLowerCase()))
+    }
+
+    renderItem(itemData: ListItemData) {
+        if (showSimpleListItem) {
+            return SimpleListItem(itemData)
+        } else {
+            return ComplexListItem(itemData)
+        }
+    }
+
+    updateSearch = (searchValue: string) => {
+        this.setState({ searchValue })
+    }
+
+    render() {
+        const { searchValue } = this.state
+        return (
+            <SafeAreaView>
+                <SearchBar value={searchValue} onChangeText={this.updateSearch} lightTheme></SearchBar>
+                <FlatList
+                    data={this.getListItems(searchValue)}
+                    renderItem={({ item }) => this.renderItem(item)}
+                />
+            </SafeAreaView>
+        );
     }
 }
-
-const MainScreen = () => {
-    return (
-        <SafeAreaView>
-            <FlatList
-                data={getListItems()}
-                renderItem={({ item }) => renderItem(item)}
-            />
-        </SafeAreaView>
-    );
-}
-
-export default MainScreen

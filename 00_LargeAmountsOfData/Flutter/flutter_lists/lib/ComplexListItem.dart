63a8b7e2738635f866c71e987bf8834ed0f8a727
Ruben Röhner <ru.roehner@gmail.com>
2022-10-24 09:58:09 +0200

    flutter_lists: Add search bar and filter logic

--- 00_LargeAmountsOfData/Flutter/flutter_lists/lib/ComplexListItem.dart
+++ 00_LargeAmountsOfData/Flutter/flutter_lists/lib/ComplexListItem.dart
@@ -3,25 +3,30 @@ import 'package:flutter/material.dart';
 class ComplexListItem extends StatelessWidget {
   const ComplexListItem(
       {super.key,
-      required this.title,
-      required this.subtitle,
+      required this.itemdata,
       required this.trailingIcon,
       required this.onButtonClicked});
 
-  final String title;
-  final String subtitle;
+  final ListitemData itemdata;
   final IconData trailingIcon;
   final Function onButtonClicked;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(title),
-      subtitle: Text(subtitle),
+      title: Text(itemdata.title),
+      subtitle: Text(itemdata.subtitle),
       trailing: Icon(trailingIcon),
       onTap: () {
         onButtonClicked();
       },
     );
   }
+}
+
+class ListitemData {
+  final String title;
+  final String subtitle;
+
+  ListitemData(this.title, this.subtitle);
 }
--- 00_LargeAmountsOfData/Flutter/flutter_lists/lib/MainPage.dart
+++ 00_LargeAmountsOfData/Flutter/flutter_lists/lib/MainPage.dart
@@ -14,6 +14,17 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final TextEditingController _textEditingController =
+      TextEditingController(text: "");
+
+  List<ListitemData> listitems = [];
+
+  @override
+  void initState() {
+    super.initState();
+    listitems = getFilteredList(_textEditingController.text);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,35 +32,60 @@ class _MainPageState extends State<MainPage> {
         title: const Text("Flutter test largeAmountof Data"),
       ),
       backgroundColor: CupertinoColors.systemBackground,
-      body: ListView.separated(
-        itemCount: MainPage.itemCount,
-        itemBuilder: (context, index) {
-          if (widget.presentSimpleItem) {
-            return SimpleListItem(title: "Item no.$index");
-          } else {
-            return ComplexListItem(
-                title: "Item no.$index",
-                subtitle:
-                    "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
-                trailingIcon: Icons.chevron_right,
-                onButtonClicked: () {
-                  showDialog(
-                    context: context,
-                    builder: ((context) {
-                      return AlertDialog(
-                        title: const Text("Click event"),
-                        content:
-                            Text("Clicked on item with the number of $index"),
-                      );
-                    }),
-                  );
-                });
-          }
-        },
-        separatorBuilder: (context, index) {
-          return const Divider();
-        },
+      body: Column(
+        children: [
+          CupertinoSearchTextField(
+            controller: _textEditingController,
+            onChanged: (value) {
+              setState(() {
+                listitems = getFilteredList(value);
+              });
+            },
+          ),
+          Expanded(
+            child: ListView.separated(
+              itemCount: listitems.length,
+              itemBuilder: (context, index) {
+                if (widget.presentSimpleItem) {
+                  return SimpleListItem(title: listitems[index].title);
+                } else {
+                  return ComplexListItem(
+                      itemdata: listitems[index],
+                      trailingIcon: Icons.chevron_right,
+                      onButtonClicked: () {
+                        showDialog(
+                          context: context,
+                          builder: ((context) {
+                            return AlertDialog(
+                              title: const Text("Click event"),
+                              content: Text(
+                                  "Clicked on item with the number of $index"),
+                            );
+                          }),
+                        );
+                      });
+                }
+              },
+              separatorBuilder: (context, index) {
+                return const Divider();
+              },
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  List<ListitemData> getFilteredList(String filter) {
+    List<ListitemData> list = List.empty(growable: true);
+
+    for (int i = 0; i < MainPage.itemCount; i++) {
+      list.add(ListitemData("Item no.$i",
+          "Lorem ipsum dolor sit amet, consetetur sadipscing elitr."));
+    }
+    list.removeWhere((element) =>
+        !element.title.toLowerCase().contains(filter.toLowerCase()) &&
+        !element.subtitle.toLowerCase().contains(filter.toLowerCase()));
+    return list;
   }
 }

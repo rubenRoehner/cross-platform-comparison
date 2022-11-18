dd282873f49b6efe751cb3b534f3fa00c003821a
Ruben Röhner <ru.roehner@gmail.com>
2022-11-18 11:38:51 +0100

    Init projects for use case custom design

---
+++ 05_CustomDesign/Ionic/ionic_custom_design/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic_custom_design',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;

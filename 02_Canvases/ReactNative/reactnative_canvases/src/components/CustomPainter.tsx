8616460750d796310647add99567d9306ea0124a
Ruben Röhner <ru.roehner@gmail.com>
2022-12-02 11:05:54 +0100

    reactnative_canvases: first implementation

--- 02_Canvases/ReactNative/reactnative_canvases/src/components/CustomPainter.tsx
+++ 02_Canvases/ReactNative/reactnative_canvases/src/components/CustomPainter.tsx
@@ -1,42 +1,60 @@
-import { Component, useEffect, useRef } from "react";
-import Canvas, { CanvasRenderingContext2D } from 'react-native-canvas';
+import React, { Component, useEffect, useRef } from "react";
+import { Dimensions, SafeAreaView, StyleSheet, View } from "react-native";
+import Canvas, { CanvasRenderingContext2D } from "react-native-canvas"
 
-class CustomPainter extends Component {
-    clearCanvas(canvas: Canvas) {
-        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    }
+const { width, height } = Dimensions.get('window');
 
-    drawCircle(context: CanvasRenderingContext2D, x: number, y: number, radius: number, fillStyle?: CanvasRenderingContext2D["fillStyle"] | undefined, strokeStyle?: CanvasRenderingContext2D["strokeStyle"] | undefined, strokeWidth?: number | undefined) {
-        context.beginPath()
-        context.arc(x, y, radius, 0, 2 * Math.PI, false)
-        if (fillStyle) {
-            context.fillStyle = fillStyle
-            context.fill()
-        }
-        if (strokeStyle) {
-            context.strokeStyle = strokeStyle
-            if (strokeWidth) {
-                context.lineWidth = strokeWidth
-            }
-            context.stroke()
+export default class CustomPainter extends Component {
+    canvas: Canvas | null = null;
+
+    componentDidMount(): void {
+        if (this.canvas) {
+            this.canvas.width = width
+            this.canvas.height = height
+            this.componentDidUpdate()
         }
     }
 
-    handleCanvas(canvas: Canvas) {
-        if (canvas) {
-            const canvasContext = canvas.getContext('2d');
-            const width = canvas.width
-            const height = canvas.height
+    componentDidUpdate() {
+        if (this.canvas) {
+            const canvasContext = this.canvas.getContext('2d');
+            const canvasWidth = this.canvas.width
+            const canvasHeight = this.canvas.height
+            clearCanvas(this.canvas)
 
             canvasContext.fillStyle = 'green'
             canvasContext.fillRect(10, 20, 50, 80)
-            this.drawCircle(canvasContext, width / 2, height / 2, width / 2, 'red')
+
+            drawCircle(canvasContext, canvasWidth / 2, canvasHeight / 2, canvasWidth / 2, 'red')
         }
     }
 
     render() {
-        return <Canvas ref={this.handleCanvas} style={{ width: "100%", height: "100%" }} />
+        return (
+            <View style={{ width: "100%", height: "100%" }}>
+                <Canvas ref={(canvas: Canvas) => (this.canvas = canvas)} />
+            </View>
+        );
     }
 };
 
-export default CustomPainter;
+
+function clearCanvas(canvas: Canvas) {
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+}
+
+function drawCircle(context: CanvasRenderingContext2D, x: number, y: number, radius: number, fillStyle?: CanvasRenderingContext2D["fillStyle"] | undefined, strokeStyle?: CanvasRenderingContext2D["strokeStyle"] | undefined, strokeWidth?: number | undefined) {
+    context.beginPath()
+    context.arc(x, y, radius, 0, 2 * Math.PI, false)
+    if (fillStyle) {
+        context.fillStyle = fillStyle
+        context.fill()
+    }
+    if (strokeStyle) {
+        context.strokeStyle = strokeStyle
+        if (strokeWidth) {
+            context.lineWidth = strokeWidth
+        }
+        context.stroke()
+    }
+}
